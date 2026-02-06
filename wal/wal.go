3673874a5ebd5f53780b652c05a3e547b66a3b4b
suman <sumanmukherjee2001@gmail.com>
2026-02-06 21:18:16 +0530

    Add initial setup for WAL

---
+++ wal/wal.go
@@ -0,0 +1,15 @@
+package wal
+
+// TODO: Figure out what should be in here
+type Wal struct {
+}
+
+// TODO: Exported Write func
+func (w *Wal) Write() {
+
+}
+
+// TODO: Internal implementation of write func
+func (w *Wal) write() {
+
+}
