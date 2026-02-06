80ea7172c04167ec2300d325f42c39af5414528d
suman <sumanmukherjee2001@gmail.com>
2026-02-06 21:35:12 +0530

    Add WALRecord decribing a record in WAL, updated WAL

--- wal/wal.go
+++ wal/wal.go
@@ -1,15 +1,50 @@
 package wal
 
-// TODO: Figure out what should be in here
-type Wal struct {
+import (
+	"os"
+	"sync"
+)
+
+type WALRecord struct {
+	Seq   uint64
+	Op    OpType
+	Key   []byte
+	Value []byte // Nil for DELETE
 }
 
-// TODO: Exported Write func
-func (w *Wal) Write() {
+type OpType uint8
 
+const (
+	OpPut    OpType = 1
+	OpDelete OpType = 2
+)
+
+func encodeRecord(seq uint64, op OpType, key, value []byte) *WALRecord {
+	return &WALRecord{
+		Seq:   seq,
+		Op:    op,
+		Key:   key,
+		Value: value,
+	}
 }
 
-// TODO: Internal implementation of write func
-func (w *Wal) write() {
+type WAL struct {
+	fd      *os.File
+	nextSeq uint64
+	mu      sync.Mutex // Protects concurrent appends(for now we have single writer)
+}
 
+// TODO:
+// Write creates a WALRecord with the data passed and calls the internal write
+// function. If successfull, it returns the seq number, else it returns 0, error
+// describing the error that occured
+func (w *WAL) Write(key, value []byte) (uint64, error) {
+	return 0, nil
+}
+
+// TODO:
+// write is responsible for writing to the WAL.
+func (w *WAL) write(op OpType, key, value []byte) (uint64, error) {
+
+	return 0, nil
 }
