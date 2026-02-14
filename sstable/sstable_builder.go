7a4502db01a045b6fd51f91d2aaba91eba55ef0e
suman <sumanmukherjee2001@gmail.com>
2026-02-14 21:37:00 +0530

    Add sst_builder, indexEntries struct

--- sstable/sstable_builder.go
+++ sstable/sstable_builder.go
@@ -1,1 +1,33 @@
 package sstable
+
+import "os"
+
+type IndexEntries struct {
+	LastKeyOfBlock []byte
+	BlockOffset    int64
+}
+
+type SstBuilder struct {
+	fd         *os.File
+	currOffset int64
+
+	// Block
+	block struct {
+		buff              []byte
+		sizeLimit         int
+		currBlockFirstKey []byte
+		currBlockLastKey  []byte
+	}
+
+	// Index
+	indexEntries []IndexEntries
+
+	// Meta-data
+	smallesKey []byte
+	largstKey  []byte
+	entryCount int
+
+	// Flags
+	finished bool
+	closed   bool
+}
