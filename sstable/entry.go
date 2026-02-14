5841b6fee774e3b55e89d0f99b5b1ec0a7357932
suman <sumanmukherjee2001@gmail.com>
2026-02-14 21:59:38 +0530

    Add encodeEntry. Formats to the binary layout

--- sstable/entry.go
+++ sstable/entry.go
@@ -1,6 +1,17 @@
 package sstable
 
+import "encoding/binary"
+
 // Entry respresents the binary layout used for disk storage
+//
+// ----  Binary Layout ----
+//
+// | KeyLen   |
+// | ValueLen |
+// | Seq      |
+// | Kind     |
+// | Key      |
+// | Value    |
 type Entry struct {
 	KeyLen   uint32 // 4 bytes
 	ValueLen uint32 // 4 bytes
@@ -20,4 +31,42 @@ func NewEntry(seq uint64, kind uint8, key, value []byte) *Entry {
 		Value:    value,
 		Kind:     kind,
 	}
+}
+
+// EncodeEntry encodes to the binary layout given by the Entry struct
+func EncodeEntry(key, value []byte, seq uint64, kind uint8) []byte {
+	keyLen, valueLen := len(key), len(value)
+
+	// keyLen bytes + valueLen bytes + KeyLen(4 bytes) + ValueLen(4 bytes) + Seq(8 bytes)
+	// + Kind(1 byte)
+	ebLen := keyLen + valueLen + 4 + 4 + 8 + 1
+
+	eb := make([]byte, ebLen)
+	offset := 0
+
+	// KeyLen
+	binary.LittleEndian.PutUint32(eb[offset:offset+4], uint32(keyLen))
+	offset += 4
+
+	// ValueLen
+	binary.LittleEndian.PutUint32(eb[offset:offset+4], uint32(valueLen))
+	offset += 4
+
+	// Seq
+	binary.LittleEndian.PutUint64(eb[offset:offset+8], seq)
+	offset += 8
+
+	// Kind
+	eb[offset] = kind
+	offset += 1
+
+	// Key
+	copy(eb[offset:offset+keyLen], key)
+	offset += keyLen
+
+	// Value
+	copy(eb[offset:offset+valueLen], value)
+	offset += valueLen
+
+	return eb
 }
