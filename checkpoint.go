839bbb47a361f3c5cc5f388fb852f3182c67b54b
suman <sumanmukherjee2001@gmail.com>
2026-03-22 20:10:09 +0530

    feat: Added checkpoint: update and read functions

---
+++ checkpoint.go
@@ -0,0 +1,79 @@
+package storageengine
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+// Update updates the checkpoint atomically to the passed value.
+func updateCheckpoint(storageDir string, seq uint64) error {
+	cDir := filepath.Join(storageDir, "checkpoint")
+
+	err := os.MkdirAll(cDir, 0755)
+	if err != nil {
+		log.Println("[CHECKPOINT] error creating checkpoint directory:", err)
+		return err
+	}
+
+	finalPath := filepath.Join(cDir, "checkpoint")
+	tempPath := filepath.Join(cDir, "temp")
+
+	seqStr := strconv.FormatUint(seq, 10)
+
+	fd, err := os.Create(tempPath)
+	if err != nil {
+		log.Printf("[CHECKPOINT] err creating temp checkpoint file, err: %v", err)
+		return err
+	}
+
+	_, err = fd.WriteString(seqStr)
+	if err != nil {
+		log.Printf("[CHECKPOINT] err writing to temp checkpoint file, err: %v", err)
+		return err
+	}
+
+	err = fd.Sync()
+	if err != nil {
+		log.Printf("[CHECKPOINT] error syncing file, err: %v", err)
+		fd.Close()
+		return err
+	}
+
+	fd.Close()
+
+	// Rename
+	if err := os.Rename(tempPath, finalPath); err != nil {
+		return err
+	}
+
+	dirFd, err := os.Open(cDir)
+	if err != nil {
+		return err
+	}
+	defer dirFd.Close()
+
+	return dirFd.Sync()
+}
+
+func readCheckpoint(storageDir string) (seq uint64, err error) {
+	cDir := filepath.Join(storageDir, "checkpoint")
+
+	err = os.MkdirAll(cDir, 0755)
+	if err != nil {
+		log.Println("[CHECKPOINT] error creating checkpoint directory:", err)
+		return 0, err
+	}
+
+	filePath := filepath.Join(cDir, "checkpoint")
+	fd, err := os.Open(filePath)
+	if err != nil {
+		return 0, err
+	}
+	defer fd.Close()
+
+	fmt.Fscanf(fd, "%d", &seq)
+	return seq, nil
+}
